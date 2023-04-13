bd64fca540234bcd3ff6f5d303bfd5402e74b741
Jacek Wojtuszek <jacek.wojtuszek@gmail.com>
2023-04-14 00:21:33 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/jw/zad_21_1_lista_produktow/Zad211ListaProduktowApplication.java
@@ -0,0 +1,13 @@
+package pl.jw.zad_21_1_lista_produktow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad211ListaProduktowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zad211ListaProduktowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jw/zad_21_1_lista_produktow/Zad211ListaProduktowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jw.zad_21_1_lista_produktow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zad211ListaProduktowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
